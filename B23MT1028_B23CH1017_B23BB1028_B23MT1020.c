3e81d9b98a39dd9d546b508c12cdbf9a2e787d34
ANAKINCS <142080298+ANAKINCS@users.noreply.github.com>
2023-12-05 11:58:22 +0530

    Update B23MT1028_B23CH1017_B23BB1028_B23MT1020.c

--- B23MT1028_B23CH1017_B23BB1028_B23MT1020.c
+++ B23MT1028_B23CH1017_B23BB1028_B23MT1020.c
@@ -1,17 +1,18 @@
-#include <stdio.h> 
+#include <stdio.h>
 
-struct Candidates{ 
- char name[30]; 
-int age; 
-int votes; 
+struct Candidates {
+    char name[30];
+    int age;
+    int votes;
 };
 
+int draw;
 void candInfo(struct Candidates* cand);
 void dispCands(struct Candidates* cand_list, int n);
 void castVote(struct Candidates* cand_list, int n);
 void dispCandsNameOnly(struct Candidates* cand_list, int n);
 int maxVote(struct Candidates* cand_list, int n);
-void result(struct Candidates* cand_list, int n);
+void result(struct Candidates* cand_list, int n, int *d);
 
 int main() {
     int n;
@@ -20,7 +21,7 @@ int main() {
 
     struct Candidates cand_list[n];
 
-    for(int i=0; i<n; i++) {
+    for (int i = 0; i < n; i++) {
         printf("please fill Candidate no.%d info:\n", i + 1);
         candInfo(&cand_list[i]);
     }
@@ -38,7 +39,7 @@ int main() {
 
         if (key == 2) {
             dispCands(cand_list, n);
-            result(cand_list, n);
+            result(cand_list, n, &draw);
             break;
         }
 
@@ -46,9 +47,10 @@ int main() {
             break;
         }
     }
+    return 0;
 }
 
-void candInfo(struct Candidates *cand) {
+void candInfo(struct Candidates* cand) {
     char ch, read = 0;
     scanf("%c", &ch);
     printf("Name :");
@@ -62,10 +64,10 @@ void candInfo(struct Candidates *cand) {
 }
 
 void dispCands(struct Candidates* cand_list, int n) {
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         printf("\nCANDIDATE NO.%d>>\n", i + 1);
         printf("NAME :");
-        for(int j = 0; cand_list[i].name[j] != '\n'; j++) {
+        for (int j = 0; cand_list[i].name[j] != '\n'; j++) {
             printf("%c", cand_list[i].name[j]);
         }
         printf("\nAGE :%d", cand_list[i].age);
@@ -75,9 +77,9 @@ void dispCands(struct Candidates* cand_list, int n) {
 
 void dispCandsNameOnly(struct Candidates* cand_list, int n) {
     printf("\n");
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         printf("%d> ", i + 1);
-        for(int j = 0; cand_list[i].name[j] != '\n'; j++) {
+        for (int j = 0; cand_list[i].name[j] != '\n'; j++) {
             printf("%c", cand_list[i].name[j]);
         }
         printf("\n");
@@ -90,11 +92,11 @@ void castVote(struct Candidates* cand_list, int n) {
     printf("Enter Cand. no :");
     scanf("%d", &cand_no);
 
-    if(!(cand_no > n || cand_no <= 0)) {
+    if (!(cand_no > n || cand_no <= 0)) {
         cand_list[cand_no - 1].votes++;
     }
 
-    if((cand_no > n || cand_no < 0)) {
+    if ((cand_no > n || cand_no < 0)) {
         printf("Wrong Candidate Number!!");
     }
 }
@@ -102,21 +104,34 @@ void castVote(struct Candidates* cand_list, int n) {
 int maxVote(struct Candidates* cand_list, int n) {
     int maxValue = cand_list[0].votes;
     int maxIndex = 0;
+    draw = 0;
 
-    for(int i = 0; i < n; i++) {
-        if(cand_list[i].votes > maxValue) {
+    for (int i = 1; i < n; i++) {
+        if (cand_list[i].votes > maxValue) {
             maxValue = cand_list[i].votes;
             maxIndex = i;
+            draw = 0;
+        } else if (cand_list[i].votes == maxValue) {
+            draw = 1;
         }
     }
-    return(maxIndex + 1);
+    if (draw) {
+        printf("There is a draw among candidates with %d votes.\n", maxValue);
+        return -1;
+    } else {
+        printf("Candidate %d wins with %d votes.\n", maxIndex + 1, maxValue);
+        return maxIndex + 1;
+    }
+    return (maxIndex + 1);
 }
 
-void result(struct Candidates* cand_list, int n) {
+void result(struct Candidates* cand_list, int n, int* d) {
     int winnerIndex = maxVote(cand_list, n);
-    printf("\nWINNER :");
-    for(int j = 0; cand_list[winnerIndex - 1].name[j] != '\n'; j++) {
-        printf("%c", cand_list[winnerIndex - 1].name[j]);
+    if (*d == 0) {
+        printf("\nWINNER :");
+        for (int j = 0; cand_list[winnerIndex - 1].name[j] != '\n'; j++) {
+            printf("%c", cand_list[winnerIndex - 1].name[j]);
+        }
+        printf("\n");
     }
-    printf("\n");
 }
